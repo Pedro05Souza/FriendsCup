55fc655ce33af8232a4f40c157072d64b4345ab4
Pedro <pedroh.fsouzars@gmail.com>
2025-07-15 18:00:11 -0300

    refact: remove position attribute

---
+++ prisma/migrations/20250715200016_remove_position/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `position` on the `group_player` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "group_player" DROP COLUMN "position";
