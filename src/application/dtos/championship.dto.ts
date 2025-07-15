92d54fbf1414db2b781f812c3ceb8b99f651556e
Pedro <pedroh.fsouzars@gmail.com>
2025-07-15 18:00:31 -0300

    refact: change championship dto

--- src/application/dtos/championship.dto.ts
+++ src/application/dtos/championship.dto.ts
@@ -1,14 +1,16 @@
 import { z } from 'zod';
 import { createZodDto } from 'nestjs-zod';
-import { matchDtoSchema } from './match.dto';
 import { playerDtoSchema } from './player.dto';
+import { duoDtoSchema } from './duo.dto';
+import { matchDtoSchema } from './match.dto';
 
 export const championshipDtoSchema = z.object({
   id: z.string(),
   title: z.string(),
   createdAtIso: z.string().datetime(),
   matches: z.array(matchDtoSchema),
-  players: z.array(playerDtoSchema),
+  players: z.array(playerDtoSchema).optional(),
+  duos: z.array(duoDtoSchema).optional(),
 });
 
 export class ChampionshipDto extends createZodDto(championshipDtoSchema) {}
